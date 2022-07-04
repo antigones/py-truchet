28aff3b2815856121632edc8e6038694d97663b9
antigones <juna.salviati@gmail.com>
2022-07-04 21:49:03 +0200

    Smith tile added

--- truchet_main.py
+++ truchet_main.py
@@ -4,9 +4,13 @@ import truchet
 def main():
     how_many_tiles = 10
     of_size = 20
-    img = truchet.paint_a_truchet(how_many_tiles, of_size)
+    img = truchet.paint_a_truchet(how_many_tiles, of_size, 'simple')
     imageio.imsave("truchet.gif", img)
 
+    of_size = 21
+    img = truchet.paint_a_truchet(how_many_tiles, of_size, 'smith')
+    imageio.imsave("truchet_smith.gif", img)
+    
 
 if __name__ == '__main__':
     main()
